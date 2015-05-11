57915293bb9ded9c4583bbae5ade1fa1176517dd
movilla <moviglez@gmail.com>
2015-05-11 16:17:59 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ for(var i=0;i<nodeList.length;i++) {
       var alto = obj.offsetHeight;
       event.target.style.left = touch.pageX-(ancho/2) + 'px';
       event.target.style.top = touch.pageY-(alto/2) + 'px';
-      var posicion = Math.floor(touch.pageX-ancho);
+      var posicion = Math.floor(touch.pageX-ancho/2);
       var muestra = document.getElementById('muestra');
       muestra.innerHTML = '<a>'+posicion+'</a><br><a>'+izq.offsetLeft+'</a>';
       if ((posicion >= (izq.offsetLeft-10)) && (posicion <= (izq.offsetLeft+10))) {
