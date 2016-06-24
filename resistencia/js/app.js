75c1bf3603425050cac52203e7afed8ef7f50bb9
movilla <moviglez@gmail.com>
2016-06-24 15:42:21 +0200

    panel control sonidos

--- resistencia/js/app.js
+++ resistencia/js/app.js
@@ -13,22 +13,3 @@ for(var i=0;i<nodeList.length;i++) {
     event.preventDefault();
   }, false);
 };
-
-document.querySelector('#b01a').onclick = function () {
-        var audio = confirm('¿Quieres comenzar con la narración?');
-        if (audio == true) {
-                document.getElementById('sonido_1').play();
-                var activar = document.getElementById('b01a');
-                activar.style.visibility ='hidden';  
-                document.querySelector('#b02a').style.visibility ='visible';
-        }
-        else { 
-                alert('Cancelado')
-        }
-};
-
-document.querySelector('#b02a').onclick = function () {
-                document.getElementById('sonido_1').pause();
-                document.querySelector('#b02a').style.visibility ='hidden';
-                document.querySelector('#b01a').style.visibility ='visible';    
-};
